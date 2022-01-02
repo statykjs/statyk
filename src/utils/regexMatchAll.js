ccb0356b5e69b305a5a54ba0b4083f69e932c92f
Anurag <hazru.anurag@gmail.com>
2022-01-02 22:16:08 +0530

    feat: added includes & interpolation

---
+++ src/utils/regexMatchAll.js
@@ -0,0 +1,26 @@
+/**
+ *
+ * @param {RegExp} regex
+ * @param {string} template
+ * @returns {{matches: string[], template: string}}
+ */
+const regexMatchAll = (regex, template) => {
+  /** allMatches holds all the results of RegExp.exec() */
+  const allMatches = [];
+  let match = regex.exec(template);
+  if (!match) {
+    return { matches: [], input: template };
+  }
+
+  const { input } = match;
+
+  while (match !== null) {
+    delete match.input;
+    allMatches.push(match);
+    match = regex.exec(template);
+  }
+
+  return { matches: allMatches, input };
+};
+
+export default regexMatchAll;
--- src/utils/resolveIncludes.js
+++ src/utils/resolveIncludes.js
@@ -1,6 +1,31 @@
 import path from "node:path";
 import fs from "node:fs";
 import { parse } from "node-html-parser";
+import { NodeVM } from "vm2";
+import logger from "./logger";
+import regexMatchAll from "./regexMatchAll";
+
+/**
+ * @param {string} js
+ * @returns {string}
+ */
+const runExpression = (js, globalVars = {}) => {
+  try {
+    const vm = new NodeVM({
+      require: {
+        external: true,
+      },
+    });
+
+    return vm.run(`
+    const props = {${Object.keys(globalVars)
+      .map((v) => `${v}: "${globalVars[v]}",`)
+      .join("\n")}};
+    module.exports = ${js}`);
+  } catch (err) {
+    logger.warn(err);
+  }
+};
 
 /**
  *
@@ -8,7 +33,7 @@ import { parse } from "node-html-parser";
  * @param {string} baseFolder
  * @returns
  */
-const resolveIncludes = (html, baseFolder) => {
+const resolveIncludes = (html, baseFolder, vars = {}) => {
   const root = parse(html);
   const includes = root.querySelectorAll("include[src]");
 
@@ -17,7 +42,33 @@ const resolveIncludes = (html, baseFolder) => {
     const parsedUrl = path.resolve(baseFolder, url);
     const html = fs.readFileSync(parsedUrl, { encoding: "utf-8" });
     const nestedUrl = path.join(baseFolder, path.dirname(url));
-    include.replaceWith(resolveIncludes(html, nestedUrl));
+
+    const CURLY_REGEX = /\{\{([^}]+)\}\}/;
+    const CURLY_REGEX_ALL = /\{\{([^}]+)\}\}/gim;
+    Object.keys(include.attributes).forEach((attr) => {
+      const match = include.attributes[attr].match(CURLY_REGEX);
+      if (attr && match) {
+        include.setAttribute(attr, runExpression(match[1], vars));
+      }
+    });
+
+    function interpolateCurly(html, attrs) {
+      const { matches } = regexMatchAll(CURLY_REGEX_ALL, html);
+      matches.forEach((match) => {
+        const expression = match[1];
+        html = html.replace(match[0], runExpression(expression, attrs));
+      });
+      return html;
+    }
+
+    include.replaceWith(
+      parse(
+        interpolateCurly(
+          resolveIncludes(html, nestedUrl, include.attributes),
+          include.attributes
+        )
+      )
+    );
   });
 
   return root.innerHTML;
