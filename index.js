2bb3635c2fb9166e3c09426a2af1e8b888a00053
Anurag Hazra <hazru.anurag@gmail.com>
2022-01-29 11:39:43 +0530

    test: added more tests for compileTemplate & fix mergedProps (#9)
    * chore: update lockfile
    
    * test: added more tests for compileTemplate & fix mergedProps

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import Statyk from "./dist";
-import fetch from "cross-fetch";
-import take from "lodash/take";
+import Statyk from "./dist/index.mjs";
+// import fetch from "cross-fetch";
+// import take from "lodash/take";
 
 const statyk = new Statyk();
 
@@ -10,47 +10,64 @@ statyk.init({
   pagesFolder: "pages",
 });
 
-async function sourceDevTo() {
-  const API_BASE = "https://dev.to/api/articles";
-  const articles = await fetch(`${API_BASE}?username=anuraghazra`).then((res) =>
-    res.json()
-  );
+// async function sourceDevTo() {
+//   const API_BASE = "https://dev.to/api/articles";
+//   const articles = await fetch(`${API_BASE}?username=anuraghazra`).then((res) =>
+//     res.json()
+//   );
 
-  const pageNodes = await Promise.all(
-    take(articles, 3).map(async (article) => {
-      const { body_markdown } = await fetch(`${API_BASE}/${article.id}`).then(
-        (res) => res.json()
-      );
+//   const pageNodes = await Promise.all(
+//     take(articles, 3).map(async (article) => {
+//       const { body_markdown } = await fetch(`${API_BASE}/${article.id}`).then(
+//         (res) => res.json()
+//       );
 
-      return {
-        path: `pages/${article.slug}.md`,
-        content: `<include src="partials/layout.html" title={{props.title}}>\n${body_markdown}\n</include>`,
-        context: { title: article.title },
-      };
-    })
-  );
+//       return {
+//         path: `pages/${article.slug}.md`,
+//         content: `<include src="partials/layout.html" title={{props.title}}>\n${body_markdown}\n</include>`,
+//         context: { title: article.title },
+//       };
+//     })
+//   );
 
-  statyk.createPages(pageNodes);
-}
+//   statyk.createPages(pageNodes);
+// }
 
-/** @type {import("./src/core/types").StatykPlugin} */
-function statykPlugin() {
-  return {
-    beforeBuild: sourceDevTo,
-    afterBuild: () => {
-      console.log("Build done");
-    },
-    beforeCreatePage: async (node, buildInfo) => {
-      console.log(`beforeCreatePage for ${node.fileName}`);
-    },
-    afterCreatePage: async (node, buildInfo) => {
-      console.log(`afterCreatePage for ${node.fileName}`);
-    },
-    beforeStaticCopy() {},
-    afterStaticCopy() {},
-  };
-}
+// /** @type {import("./src/core/types").StatykPlugin} */
+// function statykPlugin() {
+//   return {
+//     beforeBuild: sourceDevTo,
+//     afterBuild: () => {
+//       console.log("Build done");
+//     },
+//     beforeCreatePage: async (node, buildInfo) => {
+//       console.log(`beforeCreatePage for ${node.fileName}`);
+//     },
+//     afterCreatePage: async (node, buildInfo) => {
+//       console.log(`afterCreatePage for ${node.fileName}`);
+//     },
+//     beforeStaticCopy() {},
+//     afterStaticCopy() {},
+//   };
+// }
 
-statyk.use(statykPlugin());
+// data: 1
+// [inc, inc]
+// eval prop -> [2]
+// -> child -> renders props.data
+// -> eval prop -> [2]
+statyk.use({
+  beforeBuild(buildInfo) {
+    statyk.createPage({
+      path: "./dev.html",
+      buildInfo,
+      content: `
+        <include src="partials/test.html" data="{{parseInt(props.data)+1}}" />
+        <include src="partials/test.html" data="{{parseInt(props.data)+1}}" />
+      `,
+      context: { data: 1 },
+    });
+  },
+});
 
 statyk.build();
--- tests/compileTemplate.test.ts
+++ tests/compileTemplate.test.ts
@@ -1,6 +1,14 @@
 import dedent from "dedent";
 import compileTemplate from "../src/core/compileTemplate";
 
+jest.mock("shortid", () => {
+  let count = 0
+  return { generate: jest.fn(() => {
+    count++;
+    return count;
+  }) };
+});
+
 describe("compileTemplate", () => {
   it("should render html", async () => {
     const html = `<p>{{props.msg}}</p>`;
@@ -49,6 +57,84 @@ describe("compileTemplate", () => {
     `;
 
     const res = await compileTemplate(html, "./", { add: 1 });
+    expect(res.innerHTML).toMatchSnapshot();
+  });
+
+  it("should render with include", async () => {
+    const html = dedent`
+      <include src="fixtures/test.html" data="{{props.data}}" />
+    `;
+
+    const res = await compileTemplate(html, "./tests", { data: 1 });
+    expect(res.innerHTML).toMatchSnapshot();
+  });
+
+  it("should render with nested includes", async () => {
+    const html = dedent`
+      <include src="fixtures/nested-1.html" root="{{props.msg}}" />
+    `;
+
+    const res = await compileTemplate(html, "./tests", { msg: "root" });
+    expect(res.innerHTML).toMatchSnapshot();
+  });
+
+  it("should render with multiple includes", async () => {
+    const html = dedent`
+      <include src="fixtures/test.html" data="{{parseInt(props.data)+1}}" />
+      <include src="fixtures/test.html" data="{{parseInt(props.data)+2}}" />
+    `;
+
+    const res = await compileTemplate(html, "./tests", { data: 1 });
+    expect(res.innerHTML).toMatchSnapshot();
+  });
+
+  test("frontmatter codeblock should take props", async () => {
+    const html = dedent`
+      ---
+      const url: string = 'https://nice.com/' + props.username;
+      return { url };
+      ---
+      <p>{{url}}</p>
+    `;
+
+    const res = await compileTemplate(html, "./tests", {
+      username: "anuraghazra",
+    });
+    expect(res.innerHTML).toMatchSnapshot();
+  });
+
+  it("should render with partials containing frontmatter codeblock", async () => {
+    const html = dedent`
+    ---yml
+    count: 1
+    ---
+    <include src="fixtures/frontmatter-code.html" username="{{props.username + '/' + count}}" />
+    `;
+
+    const res = await compileTemplate(html, "./tests", {
+      username: "anuraghazra",
+    });
+    expect(res.innerHTML).toMatchSnapshot();
+  });
+
+  it("should render with markdown", async () => {
+    const html = dedent`
+      ### Hello {{props.msg}}
+    `;
+
+    const res = await compileTemplate(html, "./tests", { msg: "Anurag" });
+    expect(res.innerHTML).toMatchSnapshot();
+  });
+
+  test("reusable js components should work", async () => {
+    const html = dedent`
+      <body>
+        <include src="fixtures/counter.html" step={{5}} count={{1}} />
+        <include src="fixtures/counter.html" step={{10}} count={{2}} />
+      </body>
+    `;
+
+    const res = await compileTemplate(html, "./tests", {});
     expect(res.innerHTML).toMatchSnapshot();
   });
 });
